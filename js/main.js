b49c3a5c50bb47ded198da3f965c327703a9b143
Andrew Mason <andrewmasonmedia@gmail.com>
2018-10-30 17:22:56 -0400

    Added a new homepage design based on invision layouts - and using jquery mobile

--- js/main.js
+++ js/main.js
@@ -136,29 +136,29 @@ sr.reveal('.w-left', {
 
 //smooth scroll --> https://css-tricks.com/snippets/jquery/smooth-scrolling/
 
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("#banner a, #about a, #nfld a, #scotland a, #bc a, #upcoming a").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-//        window.location.hash = hash;
-      });
-    } // End if
-  });
-});
+//$(document).ready(function(){
+//  // Add smooth scrolling to all links
+//  $("#banner a, #about a, #nfld a, #scotland a, #bc a, #upcoming a").on('click', function(event) {
+//
+//    // Make sure this.hash has a value before overriding default behavior
+//    if (this.hash !== "") {
+//      // Prevent default anchor click behavior
+//      event.preventDefault();
+//
+//      // Store hash
+//      var hash = this.hash;
+//
+//      // Using jQuery's animate() method to add smooth page scroll
+//      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+//      $('html, body').animate({
+//        scrollTop: $(hash).offset().top
+//      }, 800, function(){
+//   
+//        // Add hash (#) to URL when done scrolling (default click behavior)
+////        window.location.hash = hash;
+//      });
+//    } // End if
+//  });
+//});
 
 
